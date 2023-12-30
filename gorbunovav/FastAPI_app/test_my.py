eb995caf1b9c74272c9f0b105cd9d0199edf55ac
Alexandra <alex_313@mail.ru>
2023-12-31 01:58:24 +0400

    Добавлен тест для эндпоинта перевода текста на английский язык

--- gorbunovav/FastAPI_app/test_my.py
+++ gorbunovav/FastAPI_app/test_my.py
@@ -3,6 +3,7 @@ from main import app
 import pytest
 import os
 import pandas as pd
+import requests
 
 client = TestClient(app)
 
@@ -29,3 +30,10 @@ def test_tokenize(upload_file, my_request):
     response = client.get("/tokenize/")
     assert response.status_code == 200
     assert response.json()['response'][0].strip() == "38.05"
+
+
+def test_translation_endpoint():
+    input_text = "Пример входного текста для перевода на английский."
+    response = requests.post("http://localhost:8000/translate", json={"text": input_text})
+    assert response.status_code == 200
+    assert "translation" in response.json()
